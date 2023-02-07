b9d26f9250fd1584032c17862a9c66f953d7e1da
OgurchickRick <artem.derbin.04@list.ru>
2023-02-07 20:33:57 +0300

    Подправил

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,24 +4,57 @@
     {
         static void Main(string[] args)
         {
-            int[,] nums = { { 1, 1, 7 },
-                            { 3, 4, 6 },
-                            { 5, 8, 9 } };
+            Random rnd = new Random();
+            Console.Write("Введите кол-во строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] nums = new int[rows, columns];
             int x = 0;
-            int rows = nums.GetUpperBound(0) + 1;  
-            int columns = nums.Length / rows;
-            int[] res = new int[rows];
-            for (int i = 0; i < rows; i++)
+            int[] res = new int[rows];  
+            bool isFlag = true;
+            while (isFlag)
             {
-                for (int j = 0; j < columns; j++)
+                Console.WriteLine("Выберите способ ввода: ");
+                Console.WriteLine("1 - ввод элементов в ручную");
+                Console.WriteLine("2 - заполнение массива случайными числами");
+                int wibor = Convert.ToInt32(Console.ReadLine());
+                switch (wibor)
                 {
-                    if (nums[i, j] % 2 != 0) 
-                    {
-                        x = nums[i, j];
-                    }
+                    case 1:
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < columns; j++)
+                            {
+                                Console.Write("Введите элемент массива: ");
+                                nums[i, j] = Convert.ToInt32(Console.ReadLine());
+                                if (nums[i, j] % 2 != 0)
+                                {
+                                    x = nums[i, j];
+                                }
+                            }
+                            res[i] = x;
+                            x = 0;
+                        }
+                        isFlag = false;
+                        break;
+                    case 2:
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < columns; j++)
+                            {
+                                nums[i, j] = rnd.Next(-100, 100);
+                                if (nums[i, j] % 2 != 0)
+                                {
+                                    x = nums[i, j];
+                                }
+                            }
+                            res[i] = x;
+                            x = 0;
+                        }
+                        isFlag = false;
+                        break;
                 }
-                res[i] = x;
-                x = 0;
             }
             for (int i = 0; i < res.Length; i++)
             {
