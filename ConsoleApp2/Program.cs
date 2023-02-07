140e2978bf46a68a6fd60b067763bee510d1a505
OgurchickRick <artem.derbin.04@list.ru>
2023-02-07 19:53:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] nums = { { 1, 1, 7 },
+                            { 3, 4, 6 },
+                            { 5, 8, 9 } };
+            int x = 0;
+            int rows = nums.GetUpperBound(0) + 1;  
+            int columns = nums.Length / rows;
+            int[] res = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (nums[i, j] % 2 != 0) 
+                    {
+                        x = nums[i, j];
+                    }
+                }
+                res[i] = x;
+                x = 0;
+            }
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write($"{res[i]} \t");
+            }
+        }
+    }
+}
